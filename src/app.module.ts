91609f0fd4b682a8be6046bff26e140b71ce92d1
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2026-05-02 20:18:07 +0500

    kod optimizatsiyasi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: []
+  imports: [AuthModule, UsersModule]
 })
 export class AppModule {}
